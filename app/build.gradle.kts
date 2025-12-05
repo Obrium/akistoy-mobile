7ec0e53901ab8cb6c9211f244b8bdc575d22a521
smadduff <juanpablocampinotoro@gmail.com>
2025-12-05 08:35:24 -0300

    version guard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.akiestoy.beacons"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
